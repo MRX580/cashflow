36ec9fdca3e541bd971dd2fc837cf1d83b0d53b1
Yuri <81318185+tqaxZ@users.noreply.github.com>
2021-11-17 21:35:23 +0200

    update

--- levels.py
+++ levels.py
@@ -1,13 +1,21 @@
+import math
 import random
-class level:
+
+class Level:
     def __init__(self, moves, target, salary, expenses, money):
         self.moves = moves
         self.target = target
         self.zp = salary
         self.rasxodi = expenses
         self.money = money
-    def vivod(self):
-        print('У вас %s ходов ваша цель %s\nВаша зарплата %s, расходы %s\nВаш баланс %s' % (self.moves,self.target,self.zp,self.rasxodi,self.money))
 
-levelOne = level(32, '100к', 10000, 5000, 0)
+    def vivod(self):
+        
+        print('У вас %s ходов ваша цель %s\nВаша зарплата %s, расходы %s\nВаш баланс %s' % (self.moves,self.target,self.zp,self.rasxodi,self.money))
+    def level_1(self):
+        level = 0
+
+
+
+levelOne = Level(32, '100к', random.randint(10000,20000), 5000, 0)
 levelOne.vivod()
