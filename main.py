65fc881b2e0749228735f992be9e1fc52d6891f5
Vadim <andrejchenko0511@mail.ru>
2021-11-18 21:58:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sqlite3, logging, math, os, random, datetime, config, level.levels
+import sqlite3, logging, math, os, random, datetime, config, level.choicelevel as levels
 import aiogram.utils.markdown as md
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -23,23 +23,37 @@ logging.basicConfig(level=logging.INFO)
 memory = MemoryStorage()
 bot = Bot(config.TOKEN)
 dp = Dispatcher(bot=bot, storage=memory)
+
+
 class game(StatesGroup):
     choiceLevel = State()
+    waitingGame = State()
+
+
+async def datebase_user(userid):
+    sqlite_select_query = """SELECT * FROM users"""
+    cur.execute(sqlite_select_query)
+    record = cur.fetchall()
+    print(record)
+    for i in record:
+        if i[0] == userid:
+            return i
+
+
+async def addDatebase(userid, userName, userFirst, userLast):
+    try:
+        cur.execute("INSERT INTO users VALUES(?, ?, ?, ?, ?, ?, ?, ?);", (userid, userName, userFirst, userLast, 0, False, 1, 0))
+        conn.commit()
+    except sqlite3.IntegrityError:
+        pass
+
 
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
     print('[INFO] ' + str(
         message.chat.id) + f'({message.chat.username}|{message.chat.full_name}) ' + 'написал: ' + message.text + ' ' + str(
         datetime.datetime.now()))
-    try:
-        cur.execute("INSERT INTO users VALUES(?, ?, ?, ?, ?, ?, ?, ?);", (message.chat.id, message.chat.username, message.chat.first_name, message.chat.last_name, 0, False, 1, 0))
-        conn.commit()
-    except sqlite3.IntegrityError:
-        pass
-    sqlite_select_query = """SELECT * FROM users"""
-    cur.execute(sqlite_select_query)
-    record = cur.fetchall()
-    print(record)
+    await addDatebase(message.chat.id, message.chat.username, message.chat.first_name, message.chat.last_name)
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.add('Начать игру')
     await dp.bot.set_my_commands([
@@ -59,6 +73,7 @@ async def rules(message: types.Message):
     print('[INFO] ' + str(
         message.chat.id) + f'({message.chat.username}|{message.chat.full_name}) ' + 'написал: ' + message.text + ' ' + str(
         datetime.datetime.now()))
+    await addDatebase(message.chat.id, message.chat.username, message.chat.first_name, message.chat.last_name)
     await bot.send_message(message.chat.id, md.text('coming soon'),
                            parse_mode=ParseMode.MARKDOWN)
 
@@ -68,9 +83,18 @@ async def main(message: types.Message):
     print('[INFO] ' + str(
         message.chat.id) + f'({message.chat.username}|{message.chat.full_name}) ' + 'написал: ' + message.text + ' ' + str(
         datetime.datetime.now()))
+    await addDatebase(message.chat.id, message.chat.username, message.chat.first_name, message.chat.last_name)
     if message.text.lower() == 'начать игру':
-        await bot.send_message(message.chat.id, 'Для начала вам нужно выбрать уровень')
+        await addDatebase(message.chat.id, message.chat.username, message.chat.first_name, message.chat.last_name)
+        data = await datebase_user(message.chat.id)
+        print(data[0])
+        await bot.send_message(message.chat.id, f'Для начала вам нужно выбрать уровень\nВаши доступные уровни {data[6]}')
         await game.choiceLevel.set()
+
+
+@dp.message_handler(lambda message: not message.text.isdigit(), state=game.choiceLevel)
+async def errordigit(message: types.Message):
+    await bot.send_message(message.chat.id, 'Не правильно введен уровень, повторите еще раз')
 
 
 @dp.message_handler(state=game.choiceLevel)
@@ -78,6 +102,21 @@ async def choicelevel(message: types.Message):
     print('[INFO] ' + str(
         message.chat.id) + f'({message.chat.username}|{message.chat.full_name}) ' + 'написал: ' + message.text + ' ' + str(
         datetime.datetime.now()))
-    await bot.send_message(message.chat.id, levels.Level(32, 1000).stockMarket())
+    await addDatebase(message.chat.id, message.chat.username, message.chat.first_name, message.chat.last_name)
+    data = await datebase_user(message.chat.id)
+    if int(data[6]) < int(message.text):
+        await bot.send_message(message.chat.id, 'Вам еще не доступен этот уровень')
+        return
+    await bot.send_message(message.chat.id, levels.choiceLevel(int(data[6])).work())
+    await game.waitingGame.set()
+
+
+@dp.message_handler(state=game.waitingGame)
+async def choicelevel(message: types.Message):
+    print('[INFO] ' + str(
+        message.chat.id) + f'({message.chat.username}|{message.chat.full_name}) ' + 'написал: ' + message.text + ' ' + str(
+        datetime.datetime.now()))
+
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
